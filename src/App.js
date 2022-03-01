1bce465db9ca360c0d65366034e70094a1850fc3
srihari vasupalli <vasupallisrihari@gmail.com>
2022-03-01 22:04:23 +0530

    creating new project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Game-title">Tic Tac Toe</div>
+      <header className="App-header">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
